cd81902b7a3325d3a540cd4284a15af4b7061e6a
tmwalo <tmwalo@student.wethinkcode.co.za>
2017-10-23 21:19:27 +0200

    add header

---
+++ get_next_line.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmwalo <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/06/15 16:39:09 by tmwalo            #+#    #+#             */
+/*   Updated: 2017/07/26 11:49:07 by tmwalo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 4
+# include "libft/includes/libft.h"
+
+int	get_next_line(const int fd, char **line);
+#endif
